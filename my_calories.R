2353e1d1a1b3ae30310dcd5966bfd3d5c882f9b2
Wai Lun <43715686+foowailun@users.noreply.github.com>
2018-12-16 22:47:30 +0800

    draft

--- my_calories.R
+++ my_calories.R
@@ -1,6 +1,5 @@
 # Description -------------------------------------------------------------
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# This is a Shiny web application. You can run the application by clicking the 'Run App' button above.
 
 # Library -----------------------------------------------------------------
 library(shiny)
@@ -9,118 +8,123 @@ library(data.table)
 library(ggplot2)
 
 # Data Source -------------------------------------------------------------
-
-# Data Source
-# Calories Data
-food_calories_table <- fread("food_calories_table.csv")
-# documentation_source <- fread("Documentation.txt",header = F)
-# contact_us_source <- fread("Documentation.txt", header = F)
+# Main data source
+food_calories_table <- fread("https://raw.githubusercontent.com/foowailun/alt-w/master/food_calories_table.csv")
 food_calories_table[, recommend := 0]
-my_choices <- food_calories_table[, food_short_name]
 food_products_order <- food_calories_table[, .(mean_calorie = mean(calorie_in_Kcal)), products][order(mean_calorie)][, products]
+activity_table <- as.data.table(cbind(activity_level = c("Sedentary (little or no exercise)", 
+                    "Lightly active (light exercise/sports 1-3 days/week)",
+                    "Moderately active (moderate exercise/sports 3-5 days/week)",
+                    "Very active (hard exercise/sports 6-7 days a week)",
+                    "Extra active (very hard exercise/sports & physical job or 2x training)"), 
+                  activity_multiplier = c(1.2,1.375,1.55,1.725,1.9)))
+  
+
 
 # UI ---------------------------------------------------------------
 # Define UI for application
 ui <- dashboardPage(title = "My Calories", skin = "green", 
-                    
-        dashboardHeader(title = "Dashboard", dropdownMenu(type = "tasks", badgeStatus = "success",
-                                                      taskItem(value = 90, color = "green",
-                                                               "Documentation"
-                                                      ))),
         
+        # Dashboard header                        
+        dashboardHeader(title = "My Calories App"),
+
+        # Dashboard sidebar
         dashboardSidebar(sidebarMenu(id = "sidebar",
-          menuItem("My Profile", tabName = "dashboard", icon = icon("user"),
+                                     
+          menuItem(text = "My Profile", tabName = "dashboard", icon = icon("user"),
                    badgeLabel = "Input", badgeColor = "green"),
-          # menuItem("My Meal", icon = icon("spoon"), tabName = "widgets",
-          #          badgeLabel = "Input", badgeColor = "green"),
-          menuItem("Analysis", icon = icon("bar-chart-o"), tabName = "analysis",
-                   badgeLabel = "Analysis", badgeColor = "purple"),
-          menuItem("Documentation", icon = icon("book"), tabName = "documentation",
+          
+          menuItem(text = "Recommendation", icon = icon("bar-chart-o"), tabName = "recommendation_tab",
+                   badgeLabel = "Output", badgeColor = "purple"),
+          
+          menuItem(text = "Documentation", icon = icon("book"), tabName = "documentation",
                    badgeLabel = "Info", badgeColor = "yellow"),
-          menuItem("Source code", icon = icon("code"), 
-                   href = "https://github.com/rstudio/shinydashboard/",newtab = F,
-                   badgeLabel = "GitHub", badgeColor = "red"), 
-          menuItem("Contact us", icon = icon("comment"), 
-                   href = "https://github.com/rstudio/shinydashboard/",newtab = F,
-                   badgeLabel = "Info", badgeColor = "yellow")
-        )),
+          
+          menuItem(text = "Source Code", icon = icon("code"), 
+                   href = "https://github.com/foowailun/alt-w/blob/master/my_calories.R",newtab = F,
+                   badgeLabel = "GitHub", badgeColor = "red")
+          
+          )
+        ),
         
+        # Dashboard body
         dashboardBody(
           
           tabItems(
             tabItem(tabName = "dashboard",
-                    wellPanel(radioButtons(inputId = "user_gender", label = "My Gender", choices = c("Male", "Female"), selected = NULL, inline = T),
-                              numericInput(inputId = "user_age", label = "My Age", min = 0, max = 150, step = 1, value = 0),
-                              numericInput(inputId = "user_height", label = "My Height (cm)", min = 0, max = 200, step = 1, value = 0),
-                              numericInput(inputId = "user_weight", label = "My Weight (kg)", min = 0, max = 300, step = 1, value = 0),
-                              actionButton(label = "Save & Continue", inputId = "next_button"))
+                    box(radioButtons(inputId = "user_gender", label = "My Gender", choices = c("Male", "Female"), selected = "Male", inline = T),
+                        numericInput(inputId = "user_age", label = "My Age", min = 0, max = 150, step = 1, value = 0),
+                        numericInput(inputId = "user_height", label = "My Height (cm)", min = 0, max = 300, step = 1, value = 0),
+                        numericInput(inputId = "user_weight", label = "My Weight (kg)", min = 0, max = 500, step = 1, value = 0),
+                        selectInput(label = "Activity Factor", inputId = "user_activity", choices = activity_table[, activity_level], selected = activity_table[1, activity_level]),
+                        actionButton(label = "Save & Continue", inputId = "next_button"),
+                        actionButton(label = "Reset", inputId = "reset_button")
+                        )
             ),
             
-            tabItem(tabName = "widgets",
-                    wellPanel(
+            tabItem(tabName = "recommendation_tab",
 
-                    
-                    )
-  
-            ),
-            
-            tabItem(tabName = "analysis",
-                    wellPanel(
-                      
                       fluidRow(
                         
-                        box(verbatimTextOutput("recommended_calories"),title = "Recommended Daily Calories Per Meal (Kcal)"),
-        
-                        box(actionButton(label = "Generate a random meal for me", inputId = "Generate_button"), title = "What are the recommended food for today?"),
-                        
-                        box(tableOutput("recommended_food"),title = "Recommended for you"),
-                        
-                        box(selectInput(choices =  my_choices, inputId = "user_food_intake",
-                                        label = "Customize your meal", selectize = T, multiple = T,
-                                        selected = NULL), background = "olive"),
-                        
-                        box(verbatimTextOutput("balance_calories"),title = "Balance Remaining (Kcal)"),
-                        
-                        
-                        box(tableOutput("stats"), title = "Analysis", collapsible = T, solidHeader = T, background = "olive"),
+                        box(verbatimTextOutput("recommended_calories"),title = "Recommended calories per meal (Kcal)", background = "olive",
+                            actionButton(label = "Generate Random Meal", inputId = "generate_button")), 
+
+                        box(box(selectInput(choices =  food_calories_table[, food_short_name], inputId = "user_food_intake",
+                                            label = NULL, selectize = T, multiple = T,
+                                            selected = NULL), background = "red", width = NULL, title = "Recommended For You"),
+                            box(tableOutput("diet_breakdown"), background = "olive", width = NULL, title = "Diet Analysis"),
+                            box(verbatimTextOutput("balance_calories"), title = "Balance", width = NULL),        
+                            title = "Diet Breakdown", collapsible = T, solidHeader = T, background = "olive"),
                         
                         box(plotOutput("coord_polar"), title = "Visualisation", collapsible = T, background = "olive")
-                        
-                        
-                        
-                        
                       )
-                      
-                    )
                     
             ),
             
             tabItem(tabName = "documentation",
-                    h2("How to use")
+                    h1("How to use this App"),
+                    h4("Step 1"),
+                    h4("Step 2"),
+                    h4("Step 3"),
+                    h4("Step 4")
             )
-            
           )
         )   
-        
 )
     
 # Server ------------------------------------------------------------
 # Define server logic
 server <- function(input, output, session) {
   
-  observeEvent(input$Generate_button,  {
+  observeEvent(input$generate_button,  {
           
-          quota <- user_max_intake()
-          print(quota)
-          food_calories_table[, recommend := 0]
+      quota <- user_max_intake()
+      food_calories_table[, recommend := 0]
           
           for (i in 1:length(food_products_order)) {
           food_list <- food_calories_table[products == food_products_order[i], ]
             
             if (quota >= 0) {
-              food_selected <- food_list[sample(1:nrow(food_list), size = 1), food_short_name]
-              food_calories_table[food_short_name %in% food_selected, recommend := 1]
-              quota <- quota - food_list[food_short_name == food_selected, calorie_in_Kcal]
+              food_selected <- food_list[calorie_in_Kcal <= quota]
+                
+                if (nrow(food_selected) != 0){
+                  
+                  if (i == length(food_products_order)) {
+                    food_selected <- food_selected[calorie_in_Kcal >= quantile(calorie_in_Kcal, 0.9)] 
+                    food_selected <- food_selected[sample(1:nrow(food_selected) , size = 1) , food_short_name]
+                  } else {
+                    food_selected <- food_selected[sample(1:nrow(food_selected) , size = 1) , food_short_name]  
+                  }
+                  
+                    food_calories_table[food_short_name %in% food_selected, recommend := 1]
+                    quota <- quota - food_list[food_short_name == food_selected, calorie_in_Kcal]
+                    
+                } else {
+                  food_selected <- "Not recommended"
+                  quota <- quota
+                }
+               
+              
             } else {
               food_selected <- "Not recommended"
               quota <- quota
@@ -129,8 +133,9 @@ server <- function(input, output, session) {
           print(paste(food_products_order[i], food_selected))
             
           }
-          
-    output$stats <- renderTable({
+      
+    # render table for diet breakdown      
+    output$diet_breakdown <- renderTable({
         food_calories_table[recommend == 1, .(Kcal = sum(calorie_in_Kcal)), .(Products = products)]
     })
     
@@ -140,73 +145,95 @@ server <- function(input, output, session) {
     })
     
     # coord_polar 
-    output$recommended_food <- renderTable({
-      food_calories_table[recommend == 1,.(food_short_name, calorie_in_Kcal)]
-    })
-    
-    # coord_polar 
     updateSelectInput(session, inputId = "user_food_intake", selected = food_calories_table[recommend == 1,food_short_name])
-    
     
     })  
   
   output$balance_calories <- renderText({
     isolate(
       ifelse (user_gender_data() == "Male", 
-              10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5, 
-              10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)
+              round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5)), 
+              round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)))
     ) - food_calories_table[food_short_name %in% input$user_food_intake, sum(calorie_in_Kcal), ]
   })
-  
-  # documentation
-  output$documentation <- renderTable({
-    documentation_source
-  })
 
-   # user_gender data
+  observeEvent(input$user_food_intake,  {
+    
+    # render table for diet breakdown      
+    output$diet_breakdown <- renderTable({
+      food_calories_table[food_short_name %in% input$user_food_intake, .(Kcal = sum(calorie_in_Kcal)), .(Products = products)]
+    })
+    
+    # coord_polar 
+    output$coord_polar <- renderPlot({
+      ggplot(food_calories_table[food_short_name %in% input$user_food_intake], aes(fill = food_short_name, y = calorie_in_Kcal, x = "")) + geom_bar(width = 1, stat = "identity")  + coord_polar("y", start=0) + facet_wrap(~products)
+    })
+  
+    
+  })  
+  
+  
+   # get user_gender data
    user_gender_data <- reactive({
      input$user_gender
    })
    
-   # user_age data
+   # get user_age data
    user_age_data <- reactive({
      input$user_age
    })
    
-   # user_weight data
+   # get user_weight data
    user_weight_data <- reactive({
      input$user_weight
    })
    
-   # user_height data
+   # get user_height data
    user_height_data <- reactive({
      input$user_height
    })
    
-   # user_max_intake data
-   user_max_intake <- reactive({
-     ifelse (user_gender_data() == "Male",
-             10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5,
-             10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)
-     
+   # get user_activity data
+   user_activity_data <- reactive({
+     activity_table[activity_level  == input$user_activity, as.numeric(activity_multiplier)] 
    })
    
-   # recommended_calories
+   # get user_max_intake data
+   # Mifflin-St Jeor Equation formula - 3 meals per day
+   user_max_intake <- reactive({
+     ifelse (user_gender_data() == "Male",
+             round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5)),
+             round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)))
+   })
+   
+   # when next_button is clicked
    observeEvent(input$next_button,  {
+     
+     # update recommendation_calories 
      output$recommended_calories <- renderText({
        isolate(
+         # Mifflin-St Jeor Equation formula - 3 meals per day
          ifelse (user_gender_data() == "Male",
-                 10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5,
-                 10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)
+                 round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() + 5)),
+                 round(0.33 * user_activity_data() * (10 * user_weight_data() + 6.25 * user_height_data() - 5 * user_age_data() - 161)))
        )
      })
+     
+     # update recommendation_tab
+     updateTabItems(session = session, inputId = "sidebar", selected = "recommendation_tab")
 
    })
    
-   
-   observeEvent(input$next_button, {
-     updateTabItems(session, "sidebar", "analysis")
-   }) 
+   # when reset_button is clicked
+   observeEvent(input$reset_button,  {
+     
+     # update numericInput fields
+     updateNumericInput(session = session, inputId = "user_height", value = 0)
+     updateNumericInput(session = session, inputId = "user_weight", value = 0)
+     updateNumericInput(session = session, inputId = "user_age", value = 0)
+     updateSelectInput(session = session, inputId = "user_activity", selected = activity_table[1, activity_level])
+   })
+
 }
 
 
